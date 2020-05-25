fad0b72afb06d989be1c5425b8e4fce075edc288
pedrocabralia <pedrocabralia@hotmail.com>
2020-05-25 17:50:30 -0300

    primeira versão

---
+++ index.js
@@ -0,0 +1,18 @@
+var btn = document.querySelector("button").addEventListener("click", conversor);
+var valorInput = document.querySelector("#binario");
+
+
+function conversor(){
+
+
+    
+
+    
+
+   var resultado=  parseInt(valorInput.value, 2);
+
+   var valorSpam = document.querySelector("span").innerHTML = resultado; 
+
+
+
+}
