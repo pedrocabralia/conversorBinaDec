91fd4c715dbbbc182de6462700ab2ac04efdddad
pedrocabralia <pedrocabralia@hotmail.com>
2020-05-26 11:57:40 -0300

    colocando fundo

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ var valorInput = document.querySelector("#binario");
 
 function conversor(){
 
-
-    
-
-    
-
    var resultado=  parseInt(valorInput.value, 2);
 
    var valorSpam = document.querySelector("span").innerHTML = resultado; 
 
+}
+
+function verificador(){
+
+
+   
 
 
 }
